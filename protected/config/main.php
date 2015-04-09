e99c726b90a568814bb7b93c3940bc2a351ba7af
Gianpaolo Scrigna <letsjump@gmail.com>
2015-04-09 15:10:40 +0200

    Auth info

--- protected/config/main.php
+++ protected/config/main.php
@@ -22,7 +22,7 @@ return array(
 	'modules'=>array(
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'300874',
+			'password'=>'',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
